820d487df232e220c38ff14b15c35f000838260e
lizeth <141792789+LizBarrios@users.noreply.github.com>
2025-04-01 16:32:45 -0700

    modified:   src/app/api/auth/register/route.js

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,34 +1,36 @@
 import { supabase } from "../../../../lib/superbase";
 import prisma from "../../../../lib/prisma";
 
+// Método POST
 export async function POST(req) {
   try {
     const body = await req.json();
     const { nombre, correo, password } = body;
 
+    // Validar que los datos no sean nulos o indefinidos
     if (!correo || !nombre || !password) {
       return new Response(JSON.stringify({ message: "Faltan datos necesarios." }), {
         status: 400,
       });
     }
 
-    // Verificación si el correo ya está registrado en Prisma
+    // Verificar si el correo ya está registrado en Prisma (tabla Usuario)
     const existingUser = await prisma.usuario.findUnique({
       where: { correo: correo },
     });
+
     if (existingUser) {
-      console.error(`El correo ${correo} ya está registrado.`);
       return new Response(JSON.stringify({ message: "El correo ya está registrado." }), {
         status: 400,
       });
     }
 
-    // Verificación si el nombre de usuario ya está en la base de datos
+    // Verificar si el nombre de usuario ya está en la base de datos
     const existingUsername = await prisma.usuario.findUnique({
       where: { usuario: nombre },
     });
+
     if (existingUsername) {
-      console.error(`El nombre de usuario ${nombre} ya está en uso.`);
       return new Response(JSON.stringify({ message: "El nombre de usuario ya está en uso." }), {
         status: 400,
       });
@@ -41,16 +43,15 @@ export async function POST(req) {
     });
 
     if (error) {
-      console.error("Error de Supabase: ", error.message);
       return new Response(JSON.stringify({ message: error.message }), { status: 400 });
     }
 
-    // Crear el usuario en Prisma
+    // Crear el usuario en Prisma (en la tabla `Usuario`)
     await prisma.usuario.create({
       data: {
         usuario: nombre,
         correo: correo,
-        password: password, // Asegúrate de encriptar la contraseña antes de guardarla
+        password: password, // Considera encriptar la contraseña antes de guardarla
         verificacion: false, // La verificación es falsa al principio
       },
     });
