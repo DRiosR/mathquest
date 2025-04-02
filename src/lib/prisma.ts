658afc3190b724ed5af1a83ebd4a24141964fa99
lizeth <141792789+LizBarrios@users.noreply.github.com>
2025-04-01 17:08:13 -0700

    modified:   src/app/api/auth/register/route.js 	modified:   src/app/api/auth/send-email/route.js 	modified:   src/lib/prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
-export default prisma;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export { prisma };
